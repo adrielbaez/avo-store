1f761d9e4bede9a8b19e0e741719872f737079d8
Adriel Baez <baez.adriel27@gmail.com>
2022-06-29 18:28:54 -0300

    [add] call component AvocadoHeader

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,11 @@
 import type { NextPage } from "next";
+import { AvocadoHeader } from "../components/home";
 import { Layout } from "../components/layouts";
 
 const Home: NextPage = () => {
   return (
     <Layout>
-      <h1>home</h1>
+      <AvocadoHeader />
     </Layout>
   );
 };
