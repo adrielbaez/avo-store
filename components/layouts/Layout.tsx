0c2141534b9ecb40d0e897edcff4a86bcfd90f39
Adriel Baez <baez.adriel27@gmail.com>
2022-06-29 16:59:56 -0300

    [fix] misspelled word

--- components/layouts/Layout.tsx
+++ components/layouts/Layout.tsx
@@ -1,5 +1,31 @@
-import React from "react";
+import Head from "next/head";
+import { Navbar } from "../ui";
 
-export const Layout = () => {
-  return <div>Layout</div>;
+interface LayoutProps {
+  children: JSX.Element;
+  title?: string;
+}
+
+export const Layout = ({ children, title }: LayoutProps) => {
+  return (
+    <>
+      <Head>
+        <title>{title || "Avo - Store"}</title>
+        <meta name="author" content="Adriel Baez" />
+        <meta name="description" content="Avocado store" />
+        <meta name="keywords" content={`${title}, avocado`} />
+      </Head>
+      <Navbar />
+
+      <main
+        style={{
+          padding: "0px 20px",
+          maxWidth: "1200px",
+          margin: "auto",
+        }}
+      >
+        {children}
+      </main>
+    </>
+  );
 };
