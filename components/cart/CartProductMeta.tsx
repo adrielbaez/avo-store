e631a5e0d510e3c2393030e2edc8b16695bcc517
Adriel Baez <baez.adriel27@gmail.com>
2022-06-30 18:59:02 -0300

    [add] Cart components with your styles

---
+++ components/cart/CartProductMeta.tsx
@@ -0,0 +1,47 @@
+import {
+  Badge,
+  Box,
+  HStack,
+  Icon,
+  Image,
+  Link,
+  Stack,
+  Text,
+  useColorModeValue as mode,
+} from "@chakra-ui/react";
+import * as React from "react";
+
+export type CartProductMetaProps = {
+  name: string;
+  description: string;
+  image: string;
+};
+
+export const CartProductMeta = (props: CartProductMetaProps) => {
+  const { image, name, description } = props;
+  return (
+    <Stack direction="row" spacing="5" width="full">
+      <Image
+        rounded="lg"
+        width="120px"
+        height="120px"
+        fit="cover"
+        src={image}
+        alt={name}
+        draggable="false"
+        loading="lazy"
+      />
+      <Box pt="4">
+        <Stack spacing="0.5">
+          <Text fontWeight="medium">{name}</Text>
+          <Text color={mode("gray.600", "gray.400")} fontSize="sm">
+            {description.slice(0, 55)}...
+          </Text>
+        </Stack>
+        <Badge colorScheme="green" fontSize={11}>
+          In Stock
+        </Badge>
+      </Box>
+    </Stack>
+  );
+};
--- pages/cart/index.tsx
+++ pages/cart/index.tsx
@@ -1,12 +1,74 @@
+import { GetStaticProps, NextPage } from "next";
+import {
+  Box,
+  Flex,
+  Heading,
+  HStack,
+  Link as ChakraLink,
+  Stack,
+  useColorModeValue as mode,
+} from "@chakra-ui/react";
 import React from "react";
 import { Layout } from "../../components/layouts";
+import { apiAvocado } from "../../components/api";
+import { AvocadosResponse, SingleAvocado } from "../../interfaces/avocados";
+import { CartItem, CartOrderSummary } from "../../components/cart";
+import Link from "next/link";
 
-const index = () => {
+export interface PropsProducts {
+  productsList: SingleAvocado[];
+}
+const index: NextPage<PropsProducts> = ({ productsList }) => {
   return (
     <Layout title="Cart - Store">
-      <h1>Cart</h1>
+      <Box
+        maxW={{ base: "3xl", lg: "7xl" }}
+        mx="auto"
+        px={{ base: "4", md: "8", lg: "12" }}
+        py={{ base: "6", md: "8", lg: "12" }}
+      >
+        <Stack
+          direction={{ base: "column", lg: "row" }}
+          align={{ lg: "flex-start" }}
+          spacing={{ base: "8", md: "16" }}
+        >
+          <Stack spacing={{ base: "8", md: "10" }} flex="2">
+            <Heading fontSize="2xl" fontWeight="extrabold">
+              Shopping Cart (3 items)
+            </Heading>
+
+            <Stack spacing="6">
+              {productsList.slice(0, 2).map((item) => (
+                <CartItem key={item.id} product={item} />
+              ))}
+            </Stack>
+          </Stack>
+
+          <Flex direction="column" align="center" flex="1">
+            <CartOrderSummary />
+            <HStack mt="6" fontWeight="semibold">
+              <p>or</p>
+              <Link href={"/"} passHref>
+                <ChakraLink color={mode("blue.500", "blue.200")}>
+                  Continue shopping
+                </ChakraLink>
+              </Link>
+            </HStack>
+          </Flex>
+        </Stack>
+      </Box>
     </Layout>
   );
+};
+
+export const getStaticProps: GetStaticProps = async (ctx) => {
+  const { data } = await apiAvocado.get<AvocadosResponse>("/avo");
+
+  return {
+    props: {
+      productsList: data.data,
+    },
+  };
 };
 
 export default index;
